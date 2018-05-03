6f3d2ea730d6804ada8c3923d047ff547afee20f
Tomminsky <tommy.clausner@gmail.com>
2018-05-04 01:08:16 +0200

    change info text

--- tc_expandVoxelSelection.m
+++ tc_expandVoxelSelection.m
@@ -1,10 +1,27 @@
 function [voxel_list_expanded]=tc_expandVoxelSelection(dim_of_volume,voxel_list_to_expand,expand_factors)
 %% [voxel_list_expanded]=tc_expandVoxelSelection(dim_of_volume,voxel_list_to_expand,expand_factors)
-% selects voxel around a given set of voxel. E.g. if a 3x3x3 box around 1
+% selects voxel around a given set of voxel. E.g. if a 3x4x2 box around central 1
 % voxel V in a 10x10x10 volume is asked to be selected, then call
-% V_new = tc_expandVoxelSelection([10,10,10],V,[3,3,3]);
+% V_new = tc_expandVoxelSelection([10,10,10],V,[3,4,2]);
 % V_new will contain 9 indices of the initial volume centering around V
-
+%
+% note: 
+% - all indices that are smaller than 1 will be set to 1
+% - all indices in a dimension that are greater than this dimension will be
+% set to the size of this dimension
+% - voxel_list_to_expand is a vector of voxel indices (not boolean indices)
+% -> use (find(boolean_indices) to provide valid values)
+% - expand_factors can be a scalar (equal in every direction or a vector of 
+% size 1x Ndim). In the second case the expansion in the respective
+% direction can be choosen independently
+% - the expansion factor will be added to both sides of the center voxel. 
+% Hence a 3x3x3 box around a voxel will be created using an expansion
+% factor of 1 -> [center - 1, center, center + 1]
+%
+% BTW: the method below is hundreds of times (~500-1000) faster than a for
+% loop. The bottleneck however is now memory. So
+% selecting an expansion factor of > 4 will result in a massive memory
+% explosion
 
 if size(voxel_list_to_expand,1)>size(voxel_list_to_expand,2)
    voxel_list_to_expand=voxel_list_to_expand'; 
