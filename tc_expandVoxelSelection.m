e4deccdd3a8516a0dc2f12ff063cf1e66fdabbc4
Tomminsky <tommy.clausner@gmail.com>
2018-05-03 22:41:01 +0200

    var name change

--- tc_expandVoxelSelection.m
+++ tc_expandVoxelSelection.m
@@ -1,24 +1,24 @@
-function [voxel_list_expanded]=tc_expandVoxelSelection(dim,voxel_list_to_expand,expand_factors)
-%% [voxel_list_expanded]=tc_expandVoxelSelection(dim,voxel_list_to_expand,expand_factors)
+function [voxel_list_expanded]=tc_expandVoxelSelection(dim_of_volume,voxel_list_to_expand,expand_factors)
+%% [voxel_list_expanded]=tc_expandVoxelSelection(dim_of_volume,voxel_list_to_expand,expand_factors)
 % selects voxel around a given set of voxel. E.g. if a 3x3x3 box around 1
 % voxel V in a 10x10x10 volume is asked to be selected, then call
 % V_new = tc_expandVoxelSelection([10,10,10],V,[3,3,3]);
 % V_new will contain 9 indices of the initial volume centering around V
 
-if numel(dim)~=numel(expand_factors) && numel(expand_factors)~=1
+if numel(dim_of_volume)~=numel(expand_factors) && numel(expand_factors)~=1
     error('Please provide valid expansion vector')
 end
 
 if numel(expand_factors)==1
-    expand_factors=ones(size(dim))*expand_factors;
+    expand_factors=ones(size(dim_of_volume))*expand_factors;
 end
 
-if numel(dim)==2
-    [seedX,seedY]=ind2sub(dim,voxel_list_to_expand);
+if numel(dim_of_volume)==2
+    [seedX,seedY]=ind2sub(dim_of_volume,voxel_list_to_expand);
     add_those=repmat(combvec(-expand_factors(1):1:expand_factors(1),-expand_factors(2):1:expand_factors(2)),1,size(voxel_list_to_expand,2));
     inds=repmat([seedX;seedY],1,size(add_those,2)/size(voxel_list_to_expand,2))+add_those;
-elseif numel(dim)==3
-    [seedX,seedY,seedZ]=ind2sub(dim,voxel_list_to_expand);
+elseif numel(dim_of_volume)==3
+    [seedX,seedY,seedZ]=ind2sub(dim_of_volume,voxel_list_to_expand);
     
     % compute all possible deviations from the center voxel
     add_those=repmat(combvec(-expand_factors(1):1:expand_factors(1),-expand_factors(2):1:expand_factors(2),-expand_factors(3):1:expand_factors(3)),1,size(voxel_list_to_expand,2));
@@ -29,13 +29,13 @@ else
     error('please provide valid dimensions')
 end
 
-for n=1:numel(dim)
-    inds(n,inds(n,:)>dim(n))=dim(n);
+for n=1:numel(dim_of_volume)
+    inds(n,inds(n,:)>dim_of_volume(n))=dim_of_volume(n);
 end
 inds(inds<1)=1;
 
-if numel(dim)==2
-    voxel_list_expanded=sub2ind(dim,inds(1,:),inds(2,:));
+if numel(dim_of_volume)==2
+    voxel_list_expanded=sub2ind(dim_of_volume,inds(1,:),inds(2,:));
 else
-    voxel_list_expanded=sub2ind(dim,inds(1,:),inds(2,:),inds(3,:));
+    voxel_list_expanded=sub2ind(dim_of_volume,inds(1,:),inds(2,:),inds(3,:));
 end
